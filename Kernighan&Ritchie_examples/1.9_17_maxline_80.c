550d9fea1091e0f3b88abb573398efd392f357ae
inwardness <trimethylfentanyl@gmail.com>
2024-01-04 01:39:14 +0300

    new file:   .vscode/settings.json 	modified:   Kernighan&Ritchie_examples/1.1_hello_world 	modified:   Kernighan&Ritchie_examples/1.1_hello_world.c 	modified:   Kernighan&Ritchie_examples/1.9_17_maxline_80 	modified:   Kernighan&Ritchie_examples/1.9_17_maxline_80.c

--- Kernighan&Ritchie_examples/1.9_17_maxline_80.c
+++ Kernighan&Ritchie_examples/1.9_17_maxline_80.c
@@ -2,32 +2,30 @@
 #define MAXLINE 1000
 #define LIMIT 10
 int fgetline(char line[], int lim);
-void copy(char from[], char to[]);
+
+void copy(char from[],char to[]);
 int main() {
     int len,i,j;
     char line[MAXLINE];
     char ML[4][MAXLINE]; 
- //   char ML2[MAXLINE];
-  //  char ML3[MAXLINE];
- //   char ML4[MAXLINE];
  	for(j=0;j<4;++j)
 		ML[j]==0;
     while ((len = fgetline(line, MAXLINE)) > 0) {
          if(len > 5){
-            for(j=0;j<4;++j){
-		 for(i=0;i<MAXLINE-1;++i)
-		if(fgetline(ML[j],MAXLINE)!=0)
-		copy(ML[j],line);	
-	 	++j;	 
-	 }
-         }
-	 }
-            printf("%s",ML);
-    
+            while (fgetline(ML[j],MAXLINE)!=0){                
+		          ++j;
+            }
+            copy(ML[j],line);	
+         }                    
+    }
+    for(j=0;j<4;++j)
+	printf("%s",ML);
     return 0;
 }
+
 int fgetline(char line[], int lim) {
-    int i, c;
+    int i;
+    char c;
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
         line[i] = c;
     if (c == '\n') {
@@ -38,9 +36,10 @@ int fgetline(char line[], int lim) {
     return i;
 }
 
-void copy(char from[],char to[]){
-	char i;
-	i=0;
-	while((from[i]=to[i])!='\0')
-	++i;
-}
+void copy(char from[], char to[])
+{
+    char i;
+    
+    while ((from[i] = to[i])!= '\0')
+
+    }
