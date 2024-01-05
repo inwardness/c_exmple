568affd3d4bbb7398d4839a96f76bf9cbc48b3f0
inwardness <trimethylfentanyl@gmail.com>
2024-01-05 19:16:22 +0300

    deleted:    Kernighan_Ritchie_examples/1.1_hello_world.c 	deleted:    Kernighan_Ritchie_examples/1.2_3_4_celsius_fahrenheit_while_float.c 	deleted:    Kernighan_Ritchie_examples/1.3_5_celsius_fahrenheit_revers.c 	deleted:    Kernighan_Ritchie_examples/1.3_celsius_fahrenheit_for.c 	deleted:    Kernighan_Ritchie_examples/1.4_celsius_with_define.c 	renamed:    Kernighan_Ritchie_examples/1.2_3_4_celsius_fahrenheit_while_float -> Kernighan_Ritchie_examples/1.5_1_getchar 	modified:   Kernighan_Ritchie_examples/1.5_1_getchar.c 	new file:   Kernighan_Ritchie_examples/1.5_1_getchar.exe 	new file:   Kernighan_Ritchie_examples/I.1_1.1_hello_world.c 	new file:   Kernighan_Ritchie_examples/I.2_1.3_celsius_fahrenheit_while_float.c 	renamed:    Kernighan_Ritchie_examples/1.2_celsius_fahrenheit_while.c -> Kernighan_Ritchie_examples/I.2_1.4_celsius_fahrenheit_while.c 	new file:   Kernighan_Ritchie_examples/I.3_1.5_celsius_fahrenheit_revers.c 	new file:   Kernighan_Ritchie_examples/I.3_celsius_fahrenheit_for.c 	new file:   Kernighan_Ritchie_examples/I.4_celsius_with_define.c 	renamed:    Kernighan_Ritchie_examples/1.1_hello_world -> Kernighan_Ritchie_examples/I.4_celsius_with_define.exe

--- Kernighan_Ritchie_examples/1.1_hello_world.c
+++
@@ -1,8 +0,0 @@
-#include  <stdio.h>
-#include <locale.h>
-	void main ()
-	{				
-		setlocale(LC_ALL,"Ru");			 /*first out program*/
-		printf("i'm try to find mnemonic code 0x21\n");
-	}
-
--- Kernighan_Ritchie_examples/1.5_1_getchar.c
+++ Kernighan_Ritchie_examples/1.5_1_getchar.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+//Input-Ouput symbols
+#include <stdio.h>
 
 int main()
-{					/*copy input to output with EOF_flag print*/
-	int c;				/*type of value*/
-	c=getchar();			/*c assigned function to get symbols in input stream*/
-	while (c!=EOF)			/*cycle work while c is not shourtkey Ctrl+D -End Of File*/
-        {				/*function intake and out take stream of symbols after press enter(new line)key*/
-        	putchar (c);
-       	        c=getchar();
-        }
+{					 /*copy input to output with EOF_flag print*/
+	int c;			 /*type of value*/
+	c = getchar();	 /*c assigned function to get symbols in input stream*/
+	while (c != EOF) /*cycle work while c is not shourtkey Ctrl+D -End Of File*/
+	{				 /*function intake and out take stream of symbols after press enter(new line)key*/
+		putchar(c);
+		c = getchar();
+	}
 }
