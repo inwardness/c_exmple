8ca5dd76154b713abf513dbe5f14f0fd9e1b8fbe
inwardness <trimethylfentanyl@gmail.com>
2024-02-17 19:52:32 +0300

    m

--- Kernighan_Ritchie_examples/0.c
+++ Kernighan_Ritchie_examples/0.c
@@ -2,14 +2,9 @@
 
 int main()
 {   
-    int i;
-    int words[10];
-    for( i = 0;i < 10; ++i)
-    words[i] = 0;
     char c;
-    while((c=getchar())!=EOF)
-    {
-      
-        if(c==' ')
-    }
+	printf("Press Ctrl+D or Ctrl+C:\n");
+	c = getchar();
+	printf(" get c= %d",c);
+
 }
