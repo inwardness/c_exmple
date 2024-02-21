c72ed7a6f90c49e01a64620021bec86e10e75bcf
inwardness <trimethylfentanyl@gmail.com>
2024-02-22 02:28:20 +0300

    KiR_above_I.9_1.16_exesize

--- Kernighan_Ritchie_examples/1.9_17_maxline_80.c
+++ Kernighan_Ritchie_examples/1.9_17_maxline_80.c
@@ -3,50 +3,47 @@
 #define LIMIT 10
 int fgetline(char line[], int lim);
 
-void copy(char from[], char to[]);
-int main()
-{
-    int len, i, j;
+void copy(char from[],char to[]);
+int main() {
+    int len,i,j;
     char line[IMAXLINE];
-    char ML[4][IMAXLINE];
-    for (j = 0; j < 4; ++j)
-        ML[j] == 0;
-    while ((len = fgetline(line, IMAXLINE)) > 0)
-    {
-        if (len > 5)
-        {
-            for (j = 0; j < 4; ++j)
-            {
-                for (i = 0; i < IMAXLINE - 1; ++i)
-                    if (fgetline(ML[j], IMAXLINE) != 0)
-                        copy(ML[j], line);
-                ++j;
-            }
-        }
-    }
-    printf("%s", ML);
-
+    char ML[4][IMAXLINE]; 
+  //  char ML3[MAXLINE];
+ //   char ML4[MAXLINE];
+ 	for(j=0;j<4;++j)
+		ML[j]==0;
+    while ((len = fgetline(line, IMAXLINE)) > 0) {
+         if(len > 5){
+            for(j=0;j<4;++j){
+		 for(i=0;i<IMAXLINE-1;++i)
+		if(fgetline(ML[j],IMAXLINE)!=0)
+		copy(ML[j],line);	
+	 	++j;	 
+	 }
+         }
+	 }
+            printf("%s",ML);
+    
     return 0;
 }
 
-int fgetline(char line[], int lim)
-{
+int fgetline(char line[], int lim) {
     int i;
     char c;
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
         line[i] = c;
-    if (c == '\n')
-    {
+    if (c == '\n') {
         line[i] = c;
         ++i;
     }
-     line[i] = '\0';
+  // line[i] = '\0';
     return i;
 }
 
 void copy(char from[], char to[])
 {
     char i;
+    
+    while ((from[i] = to[i])!= '\0')
 
-    while ((from[i] = to[i]) != '\0')
-}
+    }
--- Kernighan_Ritchie_examples/I.9_1.16_maxline_orbitry.c
+++ Kernighan_Ritchie_examples/I.9_1.16_maxline_orbitry.c
@@ -1,65 +1,73 @@
-	#include<stdio.h>
-	#define IMAXLINE 100
-		int fgetline(char length[],int max);			//function to get a sum of characters in array
-		void copy(char to[],char from[]);
-		int space(char lengths[],int max);		//function copy element by element from on array to other
-	int main()
-		{ 
- 		int len;								//length of inputs line to compare
-  		int max=0;
-  		int spc1=0;
-  		int spc2=0;	 
-  	char lengths[IMAXLINE];
-	char length[IMAXLINE];							//array to inputs character length of 100
-	char maxlength[IMAXLINE];
-	max=fgetline(length,IMAXLINE);
-	spc1=space(length,max);
-	copy(maxlength,length);	
-while((len=fgetline(length,IMAXLINE))>0)
-    { if(len==max){
-	spc2=space(length,max);	
-		 if(spc2>spc1)
-		 copy(maxlength,length);
-		  }
-        else if(len>max){
-		max=len;
-		copy(maxlength,length);
-		}
-		  
-    }
-	printf("\n%s\n",maxlength);
-	printf("symbols in first max string length:%d\n",spc1);
-	printf("symbols in equal string length:%d\n",spc2);
-	printf("symbols in row:%d\n",max);
-	
-     return 0;
-     }
+#include <stdio.h>
+#define IMAXLINE 100
+int fgetline(char lenInString[], int max);					//function for counting length string
+void copy(char to[], char from[]);							//copy function from getin massive to writeout massive
+int space(char spaceInStrings[], int max);					//function count spaces in equal string
+int main()
+{
+	int len;
+	int max = 0;
+	int spc1 = 0;
+	int spc2 = 0;
 
-int space(char lengths[],int max)
-{	int m=0;
-  	int i;
-	for (i=0;i<max-1;++i)
-	if (lengths[i]!=' ')
-	++m;
-	return m;
-	}	
-int fgetline(char length[],int maxline)
-{	int c,i;
-	int max;	
-	for(i=0;i<maxline-1&&(c=getchar())!=EOF&&c!='\n';++i)
-	length[i]=c;
-	if (c=='\n'){
-	length[i]=c;
-	++i;
+	char lenInStrings[IMAXLINE];
+	char spaceInString[IMAXLINE];
+	char MaxString[IMAXLINE];
+
+	max = fgetline(lenInStrings, IMAXLINE);
+	spc1 = space(lenInStrings, max);
+	copy(MaxString, lenInStrings);
+
+	while ((len = fgetline(lenInStrings, IMAXLINE)) > 0)
+	{
+		if (len == max)
+		{
+			spc2 = space(lenInStrings, max);
+			if (spc2 > spc1)
+				copy(MaxString, lenInStrings);
+		}
+		else if (len > max)
+		{
+			max = len;
+			copy(MaxString, lenInStrings);
+		}
 	}
-	length[i]='\0';
+	printf("\n%s\n", MaxString);
+	printf("symbols in first max string lenInStrings:%d\n", spc1);
+	printf("symbols in equal string lenInStrings:%d\n", spc2);
+	printf("symbols in row:%d\n", max);
+
+	return 0;
+}
+
+int space(char spaceInStrings[], int max)
+{
+	int m = 0;
+	int i;
+	for (i = 0; i < max - 1; ++i)
+		if (spaceInStrings[i] != ' ')
+			++m;
+	return m;
+}
+int fgetline(char lenInStrings[], int maxline)
+{
+	int c, i;
+	int max;
+	for (i = 0; i < maxline - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		lenInStrings[i] = c;
+	if (c == '\n')
+	{
+		lenInStrings[i] = c;
+		++i;
+	}
+	lenInStrings[i] = '\0';
 
 	return i;
 }
 
-void copy(char to[],char from[])
-{	char i;
-	while((to[i]=from[i])!='\0')
+void copy(char to[], char from[])
+{
+	char i;
+	while ((to[i] = from[i]) != '\0')
 		i++;
 }
-
