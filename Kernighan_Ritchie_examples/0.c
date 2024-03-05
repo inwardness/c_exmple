942b273f3706168ea9bc154111406b01d8e600bd
= <=>
2024-03-05 20:39:28 +0300

    modified:   .gitignore 	modified:   .vscode/c_cpp_properties.json 	modified:   Kernighan_Ritchie_examples/0.c 	deleted:    Kernighan_Ritchie_examples/I.9_1.18_change_spaces_and _tabs_to_one_space copy 	deleted:    Kernighan_Ritchie_examples/calculatio_of_rod_sgd.exe

--- Kernighan_Ritchie_examples/0.c
+++ Kernighan_Ritchie_examples/0.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
-
+#include<string.h>
     int main()
     {
-        int c;
-        while ((c=getchar())!=EOF)
-        {
-            printf("%d\n",c);
-        }
+        /*String Declaration*/
+        char nickanme[20];
+        
+        printf("Enter your Nick name:");
+
+        /*I am a reading the input string and storing  it in name
+         *Array name alone works as a base address of array so
+         *we can use array instead of &nickname here
+         */
+
+        scanf("%s",nickanme);
+
+        /*Display String*/
+        printf("%s",nickanme);
         
     }
