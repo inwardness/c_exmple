e675fc1fd508cee85bdaf26051ae5fb83a61b93f
= <=>
2024-03-07 19:17:25 +0300

    added

--- Kernighan_Ritchie_examples/0.c
+++ Kernighan_Ritchie_examples/0.c
@@ -1,20 +1,42 @@
 #include<stdio.h>
 #include<string.h>
-    int main()
-    {
+
+//string.h function:
+//
+    
+   // int main()
+   // {
         /*String Declaration*/
-        char nickanme[20];
+     //   char nickanme[20];
         
-        printf("Enter your Nick name:");
+     //  printf("Enter your Nick name:");
 
         /*I am a reading the input string and storing  it in name
          *Array name alone works as a base address of array so
          *we can use array instead of &nickname here
          */
 
-        scanf("%s",nickanme);
+     //  scanf("%s",nickanme);
 
         /*Display String*/
-        printf("%s",nickanme);
+     //  printf("%s",nickanme);
+    
+    //}
+
+    
+
+     int main()
+    {
+        /*String Declaration*/
+        char nickanme[20];
+
+        /*Console display using puts*/
+        puts("Enter your Nick name:");        
+
+        /*Input using gets*/
+        gets(nickanme);
+
+        puts(nickanme);
+
         
     }
--- Kernighan_Ritchie_examples/calculatio_of_rod_sgd.c
+++ Kernighan_Ritchie_examples/calculatio_of_rod_sgd.c
@@ -4,11 +4,19 @@
 int main()
 {
   float l, f, lFA, dcha, dpa, dda;
-  float v = 299792458.000;
+  float v =  299 792 458.000;
+  // inchecs
+  float l8 = 3.2;
+  float l4 = 6.4;  
+  float l3-8 = 9.5;
+  float l2 =12.7;
+  float l5-8 = 15.9;
+  float l3-4 = 19.0;
+  float l7-8 = 22.2;
   // char *setlocale(LC_ALL, "ru_RU");
   printf("l-длинна волны, v-скорость распространения э.м. волны, t -период колебаний\n");
-  printf("l-[m];v-[m/s]=%f;t-[Mhz]\n", v);
-  printf("Длинна волны:l= v/t\n");
+  printf("l-[m];v-[m/s]=%f;f-[Mhz]\n", v);
+  printf("Длинна волны:l= v/f\n");
   printf("Введите частоту для расчёта длины вибратора[f-Mhz][xx.xx]:\n ");
   while (f != EOF)
   {
@@ -18,13 +26,13 @@ int main()
     printf("Frenqucy:f = %f[Hz]\n", f);
     printf("Speed of ligth:v = %f[km/s]\n", v);
     printf("Length of wave:l = %3.6f[m]\n", l);
-    lFA = l * (5.0 / 8.0);
+    lFA = l +(l* l8);
     printf("длинна [m]:5/8-l = %f\n", lFA);
-    dpa = l * (1.0 / 2.0);
+    dpa = l + (l* l2);
     printf("Длинна полуволновой антенны в свободном пространстве [m]:1/2-l = %3.2f\n", dpa);
-    dcha = l * (1.0 / 4.0);
+    dcha = l + (l* l4);
     printf("Четверть длины волны [m]:1/4-l = %3.2f\n", dcha);
-    dda = (l * (5.0 / 8.0));
+    dda = (l + (l* l5-8));
     printf("Пять-восьмых длины антенны [m]:5/8-l = %3.2f\n", dda);
     printf("\n");
   }
