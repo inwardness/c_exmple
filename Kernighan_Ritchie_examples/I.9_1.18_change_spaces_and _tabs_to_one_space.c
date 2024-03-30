d1e0ad6a8ffcf21868487f0ea52e16eaf7b767b4
inwardness <trimethylfentanyl@gmail.com>
2024-03-30 21:06:25 +0300

    modified:   Kernighan_Ritchie_examples/I.9_1.18_change_spaces_and _tabs_to_one_space.c

--- Kernighan_Ritchie_examples/I.9_1.18_change_spaces_and _tabs_to_one_space.c
+++ Kernighan_Ritchie_examples/I.9_1.18_change_spaces_and _tabs_to_one_space.c
@@ -7,7 +7,6 @@ and to delete entirely blank lines.*/
 #define IMAXLINE 100
 #define LIMIT 10
 
-
 int fgetline(char line[], int lim);
 void clearstring(char line[], int lim);
 
@@ -22,34 +21,34 @@ int main()
     clearstring(line, IMAXLINE);
     clearstring(exchange, IMAXLINE);
 
-   /* do
-    {
-        clearstring(line, IMAXLINE);
-        //len = fgetline(line, IMAXLINE);
-     if (len == 1)
-        {
-          clearstring(line, IMAXLINE);
-        }
-    else
-        {
-          clearstring(line, IMAXLINE);
-          printstring(line, IMAXLINE);
-        }
+    /* do
+     {
+         clearstring(line, IMAXLINE);
+         //len = fgetline(line, IMAXLINE);
+      if (len == 1)
+         {
+           clearstring(line, IMAXLINE);
+         }
+     else
+         {
+           clearstring(line, IMAXLINE);
+           printstring(line, IMAXLINE);
+         }
 
-       
-    } while ((len = fgetline(line, IMAXLINE)) != 0);
-    
-   */
+
+     } while ((len = fgetline(line, IMAXLINE)) != 0);
+
+    */
     while ((len = fgetline(line, IMAXLINE)) != '0')
     {
         // spaces_and_tabs(line,exchange,IMAXLINE);
         if (len == 1)
         {
-             printf("\r");
+            printf("\r");
         }
         else
         {
-            //printf("%d",len);
+            // printf("%d",len);
 
             printstring(line, IMAXLINE);
             putchar('\n');
@@ -81,90 +80,87 @@ int fgetline(char line[], int lim)
     int i;
     char c;
     int spaceS = 0;
-    
+
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
     {
 
-      if (c == ' ' || c == '\t')
-    {
-        ++spaceS;
-        
-        if (c == '\t')
+        if (c == ' ' || c == '\t')
         {
-            c = ' ';
-        }
-         if (c == ' ')
-        {
-            if (spaceS == 1)
+            ++spaceS;
+
+            if (c == '\t')
             {
-                putchar(c);
-                line[i] = c;
+                c = ' ';
             }
-            else if (spaceS > 1)
+            if (c == ' ')
             {
-                putchar('\b');
-                --spaceS;
+                if (spaceS == 1)
+                {
+                    putchar(c);
+                    line[i] = c;
+                }
+                else if (spaceS > 1)
+                {
+                    putchar('\b');
+                    --spaceS;
+                }
             }
-            
         }
-    }
-    else   if(c == '\n') 
+        else if (c == '\n')
         {
-         line[i] = c;
-          ++i;
+            line[i] = c;
+            ++i;
         }
-    
-    else
-    {
-    
-        line[i] = c;
-        spaceS = 0;
-    }
-  
+
+        else
+        {
+
+            line[i] = c;
+            spaceS = 0;
+        }
     }
     return i;
 }
 
+/*   for (size_t j = 0; j < lim - 1 || line[j] == EOF; j++)
+   {
+       line[i] = exchange[j];
 
-     /*   for (size_t j = 0; j < lim - 1 || line[j] == EOF; j++)
-        {
-            line[i] = exchange[j];
-
-            if (line[i] == ' ' || line[i] == '\t')
-            {
-                ++spaceC;
-                if (line[i] == '\t')
-                {
-                    exchange[j] = ' ';
-                }
-                if (spaceC == 1)
-                {
-                    line[i] = exchange[j];
-                }
-                else if (spaceC > 1)
-                {
-                    --i;
-                }
-            }
-            else if (line[i] == '\n')
-            {
-                ++nL;
-                if (nL == 1)
-                {
-                    line[i] = exchange[j];
-                }
-                else if (nL > 1)
-                {
-                    --i;
-                }
-            }
-            else
-            {
-                nL = 0;
-                spaceC = 0;
-            }
-        }
-    }
-    return 0;
+       if (line[i] == ' ' || line[i] == '\t')
+       {
+           ++spaceC;
+           if (line[i] == '\t')
+           {
+               exchange[j] = ' ';
+           }
+           if (spaceC == 1)
+           {
+               line[i] = exchange[j];
+           }
+           else if (spaceC > 1)
+           {
+               --i;
+           }
+       }
+       else if (line[i] == '\n')
+       {
+           ++nL;
+           if (nL == 1)
+           {
+               line[i] = exchange[j];
+           }
+           else if (nL > 1)
+           {
+               --i;
+           }
+       }
+       else
+       {
+           nL = 0;
+           spaceC = 0;
+       }
+   }
+}
+return 0;
 }
 */
