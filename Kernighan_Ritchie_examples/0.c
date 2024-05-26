1e8a00721f39344f54d03738c2ab5a1b194cb262
inwardness <trimethylfentanyl@gmail.com>
2024-05-26 09:12:16 +0300

    1

--- Kernighan_Ritchie_examples/0.c
+++ Kernighan_Ritchie_examples/0.c
@@ -1,38 +1,52 @@
-#include <stdio.h>
-#include <string.h>
-
-// string.h function:
-//
-
-// int main()
-// {
-/*String Declaration*/
-//   char nickanme[20];
-
-//  printf("Enter your Nick name:");
-
-/*I am a reading the input string and storing  it in name
- *Array name alone works as a base address of array so
- *we can use array instead of &nickname here
+/* 
+ * Exercise 1-18
+ * Write a program to remove trailing blanks and tabs from each line of input, 
+ * and to delete entirely blank lines.
  */
 
-//  scanf("%s",nickanme);
+#include <stdio.h>
 
-/*Display String*/
-//  printf("%s",nickanme);
+#define MAXLINE 1000
 
-//}
+int get_line(char line[], int maxline);
+void trim(char s[], int length);
 
-int main()
-{
-   /*String Declaration*/
-   char nickanme[20];
+int main() {
+    int len,i;
+    char line[MAXLINE];
 
-   /*Console display using puts*/
-   printf("Enter your Nick name:");
+    while ((len = get_line(line, MAXLINE)) > 0) {
+      printf("%d",len);
+        trim(line, len);
+        if (line[0] != '\n')
+            for (size_t i = 0; i < line[i]; i++)
+            {
+               len = i;
+            }
+            
+            printf("%s", line);
+            printf("%d",len);
+    }
 
-   /*Input using gets*/
-   scanf("%s", nickanme);
+    return 0;
+}
 
-   printf("%s\n", nickanme);
+int get_line(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') s[i++] = c;
+    s[i] = '\0';
+    return i;
+}
+
+void trim(char line[], int length) {
+    int i;
+
+    for (i = length - 2; i >= 0 && (line[i] == ' ' || line[i] == '\t'); i--)
+        ;
+    line[++i] = '\n';
+    line[++i] = '\0';
+
 }
--- Kernighan_Ritchie_examples/I.9_1.16_maxline_orbitry.c
+++ Kernighan_Ritchie_examples/I.9_1.16_maxline_orbitry.c
@@ -6,24 +6,25 @@ the length of arbitrary long input lines, and as much as possible of the text. *
 int f1getline(char lenInString[], int max); // function for counting length string
 void copy(char to[], char from[]);		   // copy function from getin massive to writeout massive
 int fchars(char lenInString[], int max);   // function count spaces in equal string
-
+void fclear(char lenInString[],int max);		//clear string
 int main()
 {
 	int len = 0;   // symbols in gets string
 	int max = 0;   // value of chars in first basic string as a arbitrary
-	int chrs1 = 0; // spaces in previuos string with max chars in string
+	int chrs1 = 0; // spaces in previuos string with max chars
 	int chrs2 = 0;
 
 	char lenInStrings[SYMINSTRING];   // string for counting and retrieving chars
 								   // char SecondMaxString[SYMINSTRING]; // string for counting spaces
 	char FirstMaxString[SYMINSTRING]; // string with maximum chars
 
-	printf("print arbitry string:\n\n");
+	printf("print arbitry string:\n");
 
 	max = f1getline(lenInStrings, SYMINSTRING); // assigned maxline first inputs string
 	chrs1 = fchars(lenInStrings, max);		// counting chars in first string
 	copy(FirstMaxString, lenInStrings);		// copy string to FirstMaxString output
 	printf("symbols in string - %d; chars in string - %d;\n\n",max,chrs1);
+	fclear(lenInStrings,max);
 	while ((len = f1getline(lenInStrings, SYMINSTRING)) > 0) // while chars in string does not equal 0
 	{
 		if (len > max)
@@ -35,7 +36,7 @@ int main()
 				copy(FirstMaxString, lenInStrings);
 			}
 		}
-		if (len == max) // if value of chars condition equal max
+		else if (len == max) // if value of chars condition equal max
 		{
 			chrs2 = fchars(lenInStrings, max); // get value chars in string equal max
 
@@ -44,7 +45,7 @@ int main()
 				copy(FirstMaxString, lenInStrings); // copy getting string in FirstMaxString
 		}
 	}
-	printf("\ncerrnet arbitry string maximum length and maximum chars in length:\n%s\n", FirstMaxString);
+	printf("\ncerrent arbitry string maximum length and maximum chars in length:\n%s\n", FirstMaxString);
 
 	printf("symbols in cerrent string - %d; chars in cerrent string - %d;\n\n", max, chrs2);
 
@@ -59,6 +60,15 @@ int fchars(char lenInString[], int max)
 		if (lenInString[i] != ' ')
 			++m;
 	return m;
+}
+
+void fclear(char lenInStrings[],int max)
+{
+	for (int i = 0; i < max ; i++)
+	{
+		lenInStrings[i] = '\0';
+	}
+	
 }
 int f1getline(char lenInStrings[], int maxline)
 {
