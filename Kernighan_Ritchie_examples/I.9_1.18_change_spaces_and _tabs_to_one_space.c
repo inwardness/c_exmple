d6da432961f948c3b074babdabb85a09b00a2d2c
inwardness <trimethylfentanyl@gmail.com>
2024-05-26 09:12:16 +0300

    1

--- Kernighan_Ritchie_examples/I.9_1.18_change_spaces_and _tabs_to_one_space.c
+++ Kernighan_Ritchie_examples/I.9_1.18_change_spaces_and _tabs_to_one_space.c
@@ -3,148 +3,152 @@ and to delete entirely blank lines.*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include<string.h>
 #define COLUMNS 5
 #define SYMINLINE 40
 #define LIMITSTRINGS 10
 
-void f2getline(char line[],char exchline[],int lim);                        // function to get length cerrent length
-void copy(char from[], char to[]);                          // function of copy string do two demension arrays
-void fclearStrings(char StringMassive[COLUMNS][SYMINLINE]); // function in start of program for clear two demensional array
-void fclearString(char line[],int lim);
-void fprint(char StringMassive[COLUMNS][SYMINLINE]);
-void writeLine(char StringMassive[COLUMNS][SYMINLINE],char line[SYMINLINE], int count);
-void fprints(char line[],int lim);
-int fgetline(char line[],int lim);
+void trailing_blanks(char line[],int lim);
+int f2getline(char line[], int lim);      // function to get length cerrent length
+int f1getline(char line[], int lim);
+void fclearString(char line[], int lim);
+int fprints(char line[], int lim);
+int fgetline(char line[], int lim);
 int main()
 {
 
     char len = 0;
     int count = 0;
-
     char line[SYMINLINE];
     char exchline[SYMINLINE];
     char StringMassive[COLUMNS][SYMINLINE];
-    
-    fclearStrings(StringMassive);
-    fclearString(line,SYMINLINE);
-    fclearString(exchline,SYMINLINE);
+
+    fclearString(line, SYMINLINE);
+    fclearString(exchline, SYMINLINE);
     printf("enter string:\n");
 
     while ((len = fgetline(line, SYMINLINE)) > 0)
     {
-        fclearString(exchline,SYMINLINE);
-       f2getline(exchline,line,SYMINLINE);
-        fprints(exchline,SYMINLINE);
-        //fprints(line,SYMINLINE);
-        fclearString(line,SYMINLINE);
-       
-       
-      
-       
+       printf("characters in strings with blanks:%d\n",len);
+       trailing_blanks(line,len);
+       count = f1getline(line,SYMINLINE);
+       fprints(line,SYMINLINE);
+       printf("%d\n",count);
+       fclearString(line,SYMINLINE);
     }
-    
-
     return 0;
 }
+
+void trailing_blanks(char line[],int lim)
+{
+    int i;
+
+    for(i =  lim;i >=0 && line[i]!=' ';--i)
+    
+    line[i+1] = '\n';
+    line[i+2] = '\0';
+    
+}
+
 int fgetline(char line[], int lim)
 {
-	char c;
-	int i;
+    char c;
+    int i;
 
-	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-		line[i] = c;
-	if (c == '\n')
-	{
-		line[i] = c;
-		++i;
-
-	line[i] = '\0';
-    }
-	return i;
-}
-void writeLine(char StringMassive[COLUMNS][SYMINLINE], char line[SYMINLINE], int count)
-{
-    for (size_t i = 0; i < COLUMNS - 1; i++)
-    {
-        if (i == count)
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        line[i] = c;
+        
+    if (c == '\n')
         {
-            for (size_t j = 0; j < SYMINLINE - 1; j++)
-            {
-                StringMassive[count][j] = line[j];
-            }
+         line[i] = c;
+         ++i;
         }
-    }
+    line[i] = '\0';
+    return i;
+
 }
 
-void fclearStrings(char StringMassive[COLUMNS][SYMINLINE])
+int f1getline(char line[], int lim)
 {
-    for (__int32_t i = 0; i < COLUMNS - 1; ++i)
+    
+    int i,a;
+    a = 0;
+
+    for (i = 0; i < lim - 1 && line[i] != EOF; ++i)
+    if (line[i]!= 0 && line[i]!='\0')
     {
-        for (__int32_t j = 0; j < SYMINLINE - 1; j++)
-        {
-            StringMassive[i][j] = 0;
-        }
+        ++a;
     }
+    
+    
+    return a;
+
 }
+
+
 
 void fclearString(char line[], int lim)
 {
-    for (size_t i = 0; i < lim ; i++)
+    for (size_t i = 0; i < lim; i++)
     {
         line[i] = 0;
     }
 }
 
-void fprints(char line[],int lim)
+int f2getline(char line[], int lim)
 {
-    for (size_t i = 0; i < lim; i++)
+    int  i;
+    int spaces = 0;
+    char c;
+
+    
+    for ( i = 0; i < lim - 1 && (c = getchar())!=EOF && c !='\n' ; i++)
+    {
+        line[i] = c;
+
+        if (c == ' ' || c == '\t')
+        {
+            spaces++;
+            if (c == '\t')
+            {
+                c = ' ';
+            }
+            
+            if (spaces == 1)
+            {
+                line[i] = c;
+            }
+            else if (spaces > 1)
+            {
+                i--;
+            }
+
+        } 
+       /*  if(c == '\n' && i == 0)
+        {
+            c = '\r';
+        }
+        else if(c == '\n' && c > 1)
+        {
+            c = '\n';
+            i++;
+            line[i] = '\0';
+        } */
+        else
+            spaces = 0;
+    }    
+       
+return i;
+}
+
+int fprints(char line[],int lim)
+{
+    int i;
+    for (size_t i = 0; i < lim -1 && line[i]!='\0'; i++)
     {
         printf("%c",line[i]);
     }
-    
-}
-void fprint(char StringMassive[COLUMNS][SYMINLINE])
-{
-    for (__int32_t i = 0; i < COLUMNS-1; i++)
-    {
-        printf("\n");
-        for (__int32_t j = 0; j < SYMINLINE-1; j++)
-        {
-            printf("%c", StringMassive[i][j]);
-            
-        }
-        
-    }
-}
 
-void f2getline(char line[],char exchline[],int lim)
-{
-    int i,j;
-    int spaces = 0;
-    for ( i = 0; i < lim ; i++)
-    {
-        for ( j = 0; j < lim - 1; j++)
-        {
-           line[i] = exchline[i];
-           /* if (line[i] == ' ')
-           {
-            ++spaces;
-            if (spaces == 1)
-            {
-                line[i] = exchline[i];
-            }
-            else if(spaces > 1)
-            {            
-                i++;
-            }
-            else
-            {
-                spaces = 0;
-            } */
-           
-       }
-        
-    }
-    
-
+return i;
 }
