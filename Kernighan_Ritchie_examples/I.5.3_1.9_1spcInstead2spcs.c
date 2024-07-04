5ec31fcfbcf148e89b19849c40e32f3f27073c6a
inwardness <105006344+inwardness@users.noreply.github.com>
2024-07-04 17:03:02 +0000

    +

--- Kernighan_Ritchie_examples/I.5.3_1.9_1spcInstead2spcs.c
+++ Kernighan_Ritchie_examples/I.5.3_1.9_1spcInstead2spcs.c
@@ -4,6 +4,8 @@ int main()
 {
     int c;
     int spc = 0;
+    printf("entered a string for delete more then one space:\n");
+    printf("for exit press: Ctrl+D\n");
     while ((c = getchar()) != EOF)
     {
         if (c == ' ' && spc == 0)
