c0f172f3ef79974a614268615b0900a39113e48a
inwardness <trimethylfentanyl@gmail.com>
2024-11-07 22:20:21 +0300

    a

--- Kernighan_Ritchie_examples/I.9_1.18_change_spaces_and _tabs_to_one_space.c
+++ Kernighan_Ritchie_examples/I.9_1.18_change_spaces_and _tabs_to_one_space.c
@@ -1,10 +1,24 @@
-#define MAXLENGTH 40
+#define MAX_ROWS 10
+#define MAX_COLS 50
+
 #include <stdio.h>
 
 int main()
 {
-    char line[MAXLENGTH];
-    fgets(line,sizeof(line),stdin);
-    printf("string is:%s",line);
+    
+    char line[MAX_COLS];
+    char massivelines[MAX_ROWS][MAX_COLS];
+    int gtstring(char line[],int lim);
+    int len = 0;
+    int c = 0;
+
+    while ((len = (gtstring(line,MAX_COLS)) > -1))
+    {
+        rmtrailingblanks(line);
+        
+
+    }
+    
+
     return 0;
 }
