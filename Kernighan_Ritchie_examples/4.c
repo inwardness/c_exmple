0267a67ac227a521c102231a6bbc0b0d01b548f9
inwardness <trimethylfentanyl@gmail.com>
2025-01-15 17:58:54 +0300

    modified:   Kernighan_Ritchie_examples/4.c

--- Kernighan_Ritchie_examples/4.c
+++ Kernighan_Ritchie_examples/4.c
@@ -1,92 +1,61 @@
-#include<stdio.h>
+#include <stdio.h>
 
 #define MAXLINE 1000 /*максимальный размер вводимой строки*/
 #define MAX 20
-int get1line(char line[],int lim);
-void tabsentered(char line[],char tabsline[],int max,int lim)
-void copy(char to[],char from[]);
+int get1line(char line[], int lim);
+void tabsentered(char line[], char tabsline[], int max, int lim) void copy(char to[], char from[]);
 
 /*печать самой длиной строки*/
 int main(int argc, char const *argv[])
 {
-    int len;/*длина текущей строки*/
-    int max;/*длина максимально из просмотренных срок*/
-    char line[MAXLINE];/*текущая строка*/
-    char longest[MAXLINE];/*самая длиная строка*/
+    int len;               /*длина текущей строки*/
+    int max;               /*длина максимально из просмотренных срок*/
+    char line[MAXLINE];    /*текущая строка*/
+    char longest[MAXLINE]; /*самая длиная строка*/
     char tabsline[MAXLINE];
-        while ((len = get1line(line,MAXLINE)) > 0)
-        {
-            if (len < MAX)
-            {
-                copy(longest,line);
-                printf("%s",longest);
-            }
-            else if( len > MAX)/*была ли хоть одна строка?*/
-                {
-                    tabsenterd(line,tabsline,MAX,MAXLINE);
-                }
-            
-            printf("%s",line);
-
+    while ((len = get1line(line, MAXLINE)) > 0){
+        if (len < MAX){
+            copy(longest, line);
+            printf("%s", longest);
         }
-        
+            else if (len > MAX){
+             /*была ли хоть одна строка?*/
+            tabsenterd(line, tabsline, MAX, MAXLINE);
+        }
+
+        printf("%s", line);
+    }
+
     return 0;
 }
 
 /*getline:  читает строку в s, возвращает длину*/
-int get1line(char line[], int lim)
-{
-    int i,c;
-    for(i = 0; i < lim -1 && (c = getchar())!=EOF && c!= '\n';++i)
+int get1line(char line[], int lim){
+    int i, c;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
         line[i] = c;
-        if (c == '\n')
-        {
-            line[i] = c;
-            ++i;
-        }
-        line[i] = '\0';
-    return i;       
+    if (c == '\n'){
+        line[i] = c;
+        ++i;
+    }
+    line[i] = '\0';
+    return i;
 }
 
-void tabsentered(char line[],char tabsline[],int max,int lim)
-{
-    int i,j;
+void tabsentered(char line[], char tabsline[], int max, int lim){
+    int i, j;
     int count = 0;
     int spc = 0;
-
-    for ( i = 0; i < lim - 1; i++)
-    {
-        for ( j = 0; j < lim - 1; j++)
-        {
+    for (i = 0; i < lim - 1; i++){
+        for (j = 0; j < lim - 1; j++){
             ++count;
-            if (count < max)
+            if ()
             {
-                tabsline[j] = line[i];
-            }
-            else if (count == max)
-            {
-                if (line[i] == ' ' && line[i] == '\t')
-                {
-                    tabsline[j] = '\n';
-                    count = 0;
-                }
-                else if (line[i] != ' ' && line[i] != '\t')
-                {
-                    for ( i = line[i]; i == ; i++)
-                    {
-                        /* code */
-                    }
-                    
-                }
-                
-                
+                /* code */
             }
             
-
         }
-        
     }
-    
 }
 
 /*copy: копирует из from в to; to достаточно большой*/
@@ -94,7 +63,6 @@ void copy(char to[], char from[])
 {
     int i;
     i = 0;
-    while ((to[i] = from[i])!='\0')
-    ++i;  
-
+    while ((to[i] = from[i]) != '\0')
+        ++i;
 }
