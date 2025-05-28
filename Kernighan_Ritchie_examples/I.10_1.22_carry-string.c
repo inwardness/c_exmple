64a50caa55f4e30d6a0e2817b4028b478461fedc
inwardness <trimethylfentanyl@gmail.com>
2025-05-28 20:48:17 +0300

    modified:   Kernighan_Ritchie_examples/I.10_1.22_carry-string.c

--- Kernighan_Ritchie_examples/I.10_1.22_carry-string.c
+++ Kernighan_Ritchie_examples/I.10_1.22_carry-string.c
@@ -1,24 +1,5 @@
 #include<stdio.h>
 #define MAXLINE 1000
-<<<<<<< HEAD
-int get1line(int line[], int lim);
-int line[MAXLINE];
-int main()
-{
-    int c,i;
-    int len = 0;
-    int count = 0;
-    int tabedge = 0;
-    extern int line[];
-    extern int get1line();
-    printf("entered a length string fot tabs right edge:\n");
-    //  scanf("%d",&tabedge);
-    while ((len = get1line(line, MAXLINE)) > -1)
-    {
-            printf("%s", line);
-     
-    }
-=======
 #define N 8
 
 char line[MAXLINE];
@@ -38,7 +19,6 @@ int main(){
         {
             fold_line(line,MAXLINE);
         }
->>>>>>> 3b72dcb7c65d65d06b3775c1191b9c54b33c9903
     return 0;
 }
 
@@ -51,11 +31,6 @@ void clrlin(char line[],int lim){
     line[lim] = '\0';
 }
 
-<<<<<<< HEAD
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF; i++)
-{
-    line[i] = c; 
-=======
 int get1line(char line[], int lim){
     int i;
     char c;
@@ -70,7 +45,6 @@ int get1line(char line[], int lim){
     
     return i;     
 }
->>>>>>> 3b72dcb7c65d65d06b3775c1191b9c54b33c9903
 
 void fold_line(char line[], int lim){
     int i;
@@ -93,13 +67,6 @@ void fold_line(char line[], int lim){
                 printf("%.*s-\n",N,&line[i-N]);
             }
         }
-<<<<<<< HEAD
-        
-    line[i] = '\0';    
-}    
-    return i;
-=======
     }
     printf("%s",line);  
->>>>>>> 3b72dcb7c65d65d06b3775c1191b9c54b33c9903
 }
