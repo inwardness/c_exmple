6ac06197a5db57a974cf56a02eed5b4152596d51
inwardness <trimethylfetanyl@gmail.com>
2025-11-20 05:31:52 +0300

    add

--- Kernighan_Ritchie_examples/I.10_1.21_endtab.c
+++ Kernighan_Ritchie_examples/I.10_1.21_endtab.c
@@ -1,167 +1,96 @@
-#include <stdio.h>
-#define TAB 8  // Стопы табуляции (каждые 8 символов)
-
-void entab(const char line[]);
-int get1line(char line[], int lim);
-
-int main() {
-    char line[1000];
-    printf("Введите строку (пробелы -> табы):\n");
-    while (get1line(line, 1000) > 0) {
-        printf("Исходная:\n ");
-        for (int i = 0; line[i]; i++) {
-            if (line[i] == ' ') printf("·");  // Пробелы как ·
-            else if (line[i] == '\t') printf("[TAB]");
-            else putchar(line[i]);
-        }
-        printf("\nОбработанная:\n ");
-        entab(line);  // Выводим с визуализацией
-        printf("\n");
-    }
-    return 0;
-}
-
-int get1line(char line[], int lim) {
-    int i, c;
-    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
-        line[i] = c;
-    if (c == '\n') line[i++] = c;
-    line[i] = '\0';
-    return i;
-}
-
-void entab(const char line[]) {
-    int spacecount = 0;
-    int pos = 0;
-
-    for (int i = 0; line[i]; ++i) {
-        if (line[i] == ' ') {
-            spacecount++;
-            pos++;
-            if (pos % TAB == 0) {
-                printf("[TAB]");  // Визуализация таба
-                spacecount = 0;
-            }
-        } else {
-            // Печатаем оставшиеся пробелы (если не хватило для таба)
-            while (spacecount > 0) {
-                printf("·");
-                spacecount--;
-            }
-            putchar(line[i]);
-            if (line[i] == '\t') {
-                pos += (TAB - (pos % TAB));
-            } else {
-                pos++;
-            }
-        }
-    }
-    // Печатаем оставшиеся пробелы в конце строки
-    while (spacecount > 0) {
-        printf("·");
-        spacecount--;
-    }
-}
-/* #include <stdio.h>
-#define MAXLINE 1000
 #define TAB 8
+#define MAXLINE 1000
+#include "stdio.h"
 
-int max;
+int get1line(char line[], int lim);
+void endtab(char line[], int lim);
+void clear(char line[], int lim);
 char line[MAXLINE];
 
-int chspcs(char line[], int len);
-void clrlin(char line[], int lim);
-int get1line(char line[], int lim);
-
-int main()
-{
-    int len;
-    extern int get1line();
-    extern void clrlin(char line[], int lim);
-    printf("Enter a string for detab string:\n");
-    while ((len = get1line(line, MAXLINE)) > 0)
-    {
-
-        printf("%s", line);
-        printf("%d\n", len);
-        chspcs(line, len);
-        clrlin(line, MAXLINE);
-    }
-    return 0;
-}
-
-void clrlin(char line[], int lim)
+void clear(char line[], int lim)
 {
     int i;
-    for (i = 0; i < lim - 1; i++)
+    for (i = 0; i < lim; i++)
     {
-        line[i] = 0;
+        line[i] = '\0';
     }
 }
-
 int get1line(char line[], int lim)
 {
-
-    int i, c;
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+    char c;
+    int i;
+    for (i = 0; i < lim & ((c = getchar()) != EOF) & c != '\n'; i++)
         line[i] = c;
-    if (c == '\n')
+    if (line[i] == '\n')
     {
         line[i] = c;
         ++i;
     }
     line[i] = '\0';
-
     return i;
 }
 
-int chspcs(char line[], int len)
+void endtab(char line[], int lim)
 {
-    int i, j;
-   // int charvalue = 0;
-    int spacecount = 0;
-   // int postabs = 0;
-   // int posspace = 0;
+    int i;
+    int pos;
     int position = 0;
-    int tab = TAB;
-    for (i = 0; i < len; i++)
+    int space_count = 0;
+    int needForTabspcs = 0;
+    for (i = 0; i < lim && line[i] != '\0'; i++)
     {
-
         if (line[i] == ' ')
         {
+            space_count++;
             position++;
-            spacecount++;
-            if (position % tab == 0 && spacecount == tab)
-            {
-                putchar('\t');
-                
-            }
-            
-        }
-        else if (line[i] == '\n')
-        {
-            putchar('\n');
         }
         else
-        {   
-            
-            if ((position % tab != 0 )&&( spacecount < tab))
-            {
-                for ( j = 0; j < spacecount; j++)
-                {
-                    putchar('_');
-                    
-                }
-                
-            }
-           
-            position++;
-            printf("%c", line[i]);
-            
-        }
-       
-    }
+        {
 
-    return 0;
+            if (space_count > 0)
+              {
+                position = position - space_count;
+                while (space_count > 0)
+                {
+                    
+                    needForTabspcs = TAB - (position % TAB);
+                    
+                       if (needForTabspcs > 1 && needForTabspcs <= space_count)
+                        {
+                           putchar('\t');
+                           position = position + needForTabspcs;
+                           space_count = space_count - needForTabspcs;
+                        }
+                        else
+                        {
+                            while (space_count > 0)
+                            {                           
+                            putchar('_');
+                            space_count--;
+                            position++;
+                            }
+                        }
+                        
+                        
+                }
+            }
+            printf("%c", line[i]);
+        position++;
+        }
+    }
 }
- */
+
+int main()
+{
+    int length = 0;
+    extern int get1line(char line[], int lim);
+    extern void endtab(char line[], int lim);
+
+    printf("Enter a string:\n");
+    while ((length = get1line(line, MAXLINE)) > 0)
+    {
+        endtab(line, MAXLINE);
+        putchar('\n');
+        clear(line, MAXLINE);
+    }
+}
--- Kernighan_Ritchie_examples/endtab.c
+++ Kernighan_Ritchie_examples/endtab.c
@@ -2,70 +2,95 @@
 #define MAXLINE 1000
 #include "stdio.h"
 
-int get1line(char line[],int lim);
-void endtab(char line[],int lim);
-void clear(char line[],int lim);
+int get1line(char line[], int lim);
+void endtab(char line[], int lim);
+void clear(char line[], int lim);
 char line[MAXLINE];
 
-void clear(char line[],int lim){
+void clear(char line[], int lim)
+{
     int i;
-    for ( i = 0; i < lim; i++){
+    for (i = 0; i < lim; i++)
+    {
         line[i] = '\0';
     }
-    
 }
-int get1line(char line[],int lim){
+int get1line(char line[], int lim)
+{
     char c;
     int i;
-
-    for ( i = 0; i < lim & ((c = getchar())!=EOF) & c!='\n'; i++)
+    for (i = 0; i < lim & ((c = getchar()) != EOF) & c != '\n'; i++)
         line[i] = c;
-            if (line[i] == '\n'){
-                line[i] = c;
-                ++i;
-            }
-            line[i] = '\0';
+    if (line[i] == '\n')
+    {
+        line[i] = c;
+        ++i;
+    }
+    line[i] = '\0';
     return i;
 }
 
-void endtab(char line[],int lim){
+void endtab(char line[], int lim)
+{
     int i;
-    int CharPos = 0;
-    int SpcPos = 0;
-    for ( i = 0; i < lim; i++){
-        if(line[i] == '_'){
-            SpcPos++;
-            CharPos++;
-            if (SpcPos == TAB){
-                putchar('\t');
-                
-            }
+    int pos;
+    int position = 0;
+    int space_count = 0;
+    int needForTabspcs = 0;
+    for (i = 0; i < lim && line[i] != '\0'; i++)
+    {
+        if (line[i] == ' ')
+        {
+            space_count++;
+            position++;
         }
-        else if(line[i]!='_'){ 
-                if (SpcPos > 0){
-                    while (SpcPos > 0){
-                        SpcPos--;
-                        CharPos++;
-                        putchar('_');
-                    }
-                }else{
-                    putchar(line[i]);
-                    CharPos++;
-                } 
+        else
+        {
+
+            if (space_count > 0)
+              {
+                position = position - space_count;
+                while (space_count > 0)
+                {
+                    
+                    needForTabspcs = TAB - (position % TAB);
+                    
+                       if (needForTabspcs > 1 && needForTabspcs <= space_count)
+                        {
+                           putchar('\t');
+                           position = position + needForTabspcs;
+                           space_count = space_count - needForTabspcs;
+                        }
+                        else
+                        {
+                            while (space_count > 0)
+                            {                           
+                            putchar('_');
+                            space_count--;
+                            position++;
+                            }
+                        }
+                        
+                        
+                }
             }
+            printf("%c", line[i]);
+        position++;
         }
+    }
 }
 
-int main(){
+int main()
+{
     int length = 0;
-    extern int get1line(char line[],int lim);
-    extern void endtab(char line[],int lim);
+    extern int get1line(char line[], int lim);
+    extern void endtab(char line[], int lim);
 
     printf("Enter a string:\n");
-    while ((length = get1line(line,MAXLINE)) > 0){
-        endtab(line,MAXLINE);
+    while ((length = get1line(line, MAXLINE)) > 0)
+    {
+        endtab(line, MAXLINE);
         putchar('\n');
-        clear(line,MAXLINE);
+        clear(line, MAXLINE);
     }
-    
 }
