59c54bf7da341022353fec1f5307cc365340faa0
= <=>
2025-12-06 18:44:08 +0300

    edded endtab_1.10_1.21

--- Kernighan_Ritchie_examples/I.10_1.21_endtab.c
+++ Kernighan_Ritchie_examples/I.10_1.21_endtab.c
@@ -1,64 +1,86 @@
 #include <stdio.h>
-#define TAB 8  // Стопы табуляции (каждые 8 символов)
+#define MAXLINE 1000
+#define TAB 8  
 
-void entab(const char line[]);
-int get1line(char line[], int lim);
+void entab(char line[],int maxline);
+int get1line(char line[], int maxline);
+void cstring(char line[], int maxline);
 
 int main() {
-    char line[1000];
-    printf("Введите строку (пробелы -> табы):\n");
-    while (get1line(line, 1000) > 0) {
-        printf("Исходная:\n ");
-        for (int i = 0; line[i]; i++) {
-            if (line[i] == ' ') printf("·");  // Пробелы как ·
-            else if (line[i] == '\t') printf("[TAB]");
-            else putchar(line[i]);
-        }
-        printf("\nОбработанная:\n ");
-        entab(line);  // Выводим с визуализацией
-        printf("\n");
+    char line[MAXLINE];
+    cstring(line,MAXLINE);
+    printf("Введите строку:\n");
+    while (get1line(line, MAXLINE) > 0){
+           entab(line,MAXLINE);
+           cstring(line,MAXLINE);
+           printf("\n");  
     }
     return 0;
 }
 
-int get1line(char line[], int lim) {
+int get1line(char line[], int maxline) {
     int i, c;
-    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    for (i = 0; i < maxline && (c = getchar()) != '\0' && c!='\n'; ++i){
         line[i] = c;
-    if (c == '\n') line[i++] = c;
-    line[i] = '\0';
+    }
+        if (c == '\n'){
+            line[i] = c; 
+            i++;
+            line[i] = '\0';
+        } 
     return i;
 }
 
-void entab(const char line[]) {
+void entab(char line[], int maxline) {
     int spacecount = 0;
-    int pos = 0;
+    int position = 0;
+    int spacetoTab = 0;
 
-    for (int i = 0; line[i]; ++i) {
-        if (line[i] == ' ') {
-            spacecount++;
-            pos++;
-            if (pos % TAB == 0) {
-                printf("[TAB]");  // Визуализация таба
-                spacecount = 0;
-            }
-        } else {
-            // Печатаем оставшиеся пробелы (если не хватило для таба)
-            while (spacecount > 0) {
-                printf("·");
-                spacecount--;
-            }
-            putchar(line[i]);
-            if (line[i] == '\t') {
-                pos += (TAB - (pos % TAB));
-            } else {
-                pos++;
-            }
-        }
+    for (int i = 0; i < maxline - 1 && line[i]!= '\0' ; ++i){ 
+            if (line[i] == ' '){ 
+                spacecount++;
+                position++;
+                }else{
+                    position = position - spacecount;
+                      while (spacecount > 0){
+                            spacetoTab =TAB - (position%TAB);
+                                if (spacetoTab > 1 && spacecount >= spacetoTab){
+                                    putchar('\t');
+                                    spacecount = spacecount - spacetoTab;
+                                    position = position + spacetoTab;
+                                }else{
+                                    while (spacecount > 0){
+                                        putchar('_');
+                                        spacecount--;
+                                        position++;
+                                    }
+                                }    
+                        }
+                printf("%c",line[i]);
+                position++;       
+                }
     }
-    // Печатаем оставшиеся пробелы в конце строки
-    while (spacecount > 0) {
-        printf("·");
-        spacecount--;
+            if (spacecount > 0) {
+                position = position - spacecount;  // откат к началу пробелов
+                     while (spacecount > 0) {
+                        spacetoTab = TAB - (position % TAB);
+                            if (spacetoTab > 1 && spacecount >= spacetoTab) {
+                            putchar('\t');
+                            spacecount = spacecount - spacetoTab;
+                            position = position + spacetoTab;
+                            } else {
+                            putchar('_');
+                            spacecount--;
+                            position++;
+                            }
+                    }
+            }
+        
+}
+
+void cstring(char line[], int maxline){
+    int i;
+    for (i = 0; i < maxline - 1; i++){
+        line[i] = '\0';
     }
 }
