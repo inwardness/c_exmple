4690c6880a6d00d358c01d354fe1bcd5bdb9bf10
polyhedron <polyhedron@localhost.localdomain>
2026-02-19 21:22:01 +0300

    changed_base

--- Kernighan_Ritchie_examples/Kernighan_Ritchie_examples/EOF_1_0.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-	printf("Press any key.ENTER would be nice :-)\n\n");
-	printf("The expression getchar()!=EOF evalutes to %d\n",getchar()!=EOF);	return(0);
-}
